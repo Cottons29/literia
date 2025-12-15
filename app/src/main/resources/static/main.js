7a6b504dec57c792e01cb2a9f88362d251fed367
Cottons29 <86792298+Cottons29@users.noreply.github.com>
2025-12-15 23:50:38 +0700

    Added some template for demo (#4)

---
+++ app/src/main/resources/static/main.js
@@ -0,0 +1,36 @@
+import { page_mapping } from 'http://localhost:3000/supplier/static?filename=constants&type=js';
+async function fetch_html_fragment(url) {
+    let response = "";
+    try {
+        const res = await fetch(url, {
+            method: "GET",
+        });
+        response = await res.text();
+    }
+    catch (e) {
+        console.error(e);
+    }
+    return response;
+}
+function render_page(html, container) {
+    container.innerHTML = html;
+}
+document.addEventListener("DOMContentLoaded", function (_) {
+    console.log("DOM fully loaded and parsed");
+    const main_content = document.getElementById("main-content");
+    if (!main_content) {
+        throw new Error("main-content not found");
+    }
+    const navbar_btt = document.querySelectorAll(".functional-button");
+    navbar_btt.forEach(async (btt) => {
+        if (page_mapping.has(btt.id)) {
+            const url = `http://localhost:3000${page_mapping.get(btt.id)}`;
+            btt.addEventListener("click", async () => {
+                console.log(`Button clicked: ${btt.id}`);
+                const html = await fetch_html_fragment(url);
+                render_page(html, main_content);
+            });
+        }
+    });
+});
+//# sourceMappingURL=main.js.map
---
+++ web_scripts/src/constants.ts
@@ -0,0 +1,4 @@
+export const page_mapping: Map<string, string> = new Map([
+    ["home-button", "/home"],
+    ["tasks-button", "/tasks"],
+])
